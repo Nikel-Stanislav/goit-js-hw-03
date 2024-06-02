a611538e6c898d7335cddab7c9f86580543384a8
Nikel Stanislav <162141430+Nikel-Stanislav@users.noreply.github.com>
2024-06-02 14:05:15 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -6,4 +6,20 @@ function filterArray(numbers, value) {
   // Використай цикл для ітерації кожного елемента масиву numbers.
   // Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
   // Поверни свій новий масив з підходящими числами як результат.
+  let newFilterArray = [];
+  for (let i = 0; i <= numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      newFilterArray.push(numbers[i]);
+    }
+  }
+  return newFilterArray;
 }
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// let numbers = 0;
+// for (let i = numbers; i <= value; i += 1); --- це не вірний варіант!
